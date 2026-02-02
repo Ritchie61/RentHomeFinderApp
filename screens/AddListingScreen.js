ce1e540406a8b945197acc7ad88d9378d4044e2d
Ritchie61 <124876391+Ritchie61@users.noreply.github.com>
2026-02-02 16:07:32 +1000

    Create AddListingScreen.js

---
+++ screens/AddListingScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function AddListingScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Add Listing Form (Coming Next)</Text>
+    </View>
+  );
+}
